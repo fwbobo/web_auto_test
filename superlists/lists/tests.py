1ac4378bb1882d43b2f70df96eea1685d94c106e
aaa <88888@sad.com>
2021-07-30 20:31:58 +0800

    first view home_page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from lists.views import home_page
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')#resolve 反向解析根路径 “/”看是否能找到home_page函数
+        self .assertEqual(found.func, home_page)
