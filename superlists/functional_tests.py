4ceac09260631792b6b5c093b71afb266c2d3b8f
aaa <88888@sad.com>
2021-07-31 10:06:59 +0800

    发现了页面无法储存的bug,对代码进行了重构

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.wb.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.wb.find_element_by_id('id_list_table')
+        rows = self.wb.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])#从rows中取出row的text 然后进行assertin
+
     def test_can_start_a_list_and_retrive_it_later(self):
         #伊丽丝听说有一个很酷的在线代办事项应用
         #她去看了这个应用的首页
@@ -35,19 +40,16 @@ class NewVisitorTest(unittest.TestCase):
         #她按了回车之后，页面更新了
         #代办事项表格中显示了1：buy peacock feathers
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
-        #time.sleep(10)#在这个代码的位置停止10 s
-        table = self.wb.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-
-
-        #页面上有显示了一个文本框，可以输入其他待办事项
+        #页面上又显示了一个文本框，可以输入其他待办事项
         #她输入了use peacock to make a fly（使用孔雀羽毛做假的苍蝇）
         #伊丽丝做事很有条例
         inputbox = self.wb.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock to make a fly')
 
         #页面再次更新时，她的清单中显示了这两个待办事项
         table = self.wb.find_element_by_id('id_list_table')
